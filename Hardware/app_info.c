de3038f8985299e80babe2ed5779a7cef1913a7d
Linball <1217048954@qq.com>
2025-07-25 17:08:10 +0800

    提交

--- Hardware/app_info.c
+++ Hardware/app_info.c
@@ -17,19 +17,10 @@
 
 const char *app_name = "电池信息采集软件";
 
-
-
-typedef struct _app_info
-{
-	app_version_t version;
-	char name[100];
-	char describe[100];
-}app_info_t;
-
-
 const app_info_t app_info __attribute__((at(FLASH_BASE+APP_INFO_OFFSET))) = {
 	.version = {0,0,0,25,7,2025},
 	.name = "电池信息采集软件",
+	.describe = "天微电子——锂电",
 };
 
 
@@ -37,7 +28,9 @@ const app_info_t *factory_app_info = (app_info_t *)(FACTORY_APP_START_ADDR + APP
 const app_info_t *app1_info		   = (app_info_t *)(APP1_START_ADDR + APP_INFO_OFFSET);
 const app_info_t *app2_info		   = (app_info_t *)(APP2_START_ADDR + APP_INFO_OFFSET);
 
-
+const app_version_t *factory_version = (app_version_t *)(FACTORY_APP_START_ADDR + APP_INFO_OFFSET);
+const app_version_t *app1_version = (app_version_t *)(APP1_START_ADDR + APP_INFO_OFFSET);
+const app_version_t *app2_version = (app_version_t *)(APP2_START_ADDR + APP_INFO_OFFSET);
 
 
 
--- Hardware/app_info.h
+++ Hardware/app_info.h
@@ -13,7 +13,20 @@ typedef struct _app_version
 	uint16_t year;
 }app_version_t;
 
+typedef struct _app_info
+{
+	app_version_t version;
+	char name[100];
+	char describe[100];
+}app_info_t;
+
 extern const char *app_name;
+extern const app_info_t *factory_app_info;
+extern const app_info_t *app1_info;
+extern const app_info_t *app2_info;
+
+extern const app_version_t *factory_version;
+extern const app_version_t *app1_version;
 
 #endif
 
